05553b3551d7d837afc156cee0b7594044b5deff
Venkateshwaran <venkateshawran.venkitachalam@veneratech.com>
2026-03-02 19:54:11 +0530

    Returning a bean using controller

--- src/main/java/com/venkatesh/rest/webservices/resful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/venkatesh/rest/webservices/resful_web_services/helloworld/HelloWorldController.java
@@ -9,4 +9,8 @@ public class HelloWorldController {
     public String helloWorld() {
         return "Hello World";
     }
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
 }
