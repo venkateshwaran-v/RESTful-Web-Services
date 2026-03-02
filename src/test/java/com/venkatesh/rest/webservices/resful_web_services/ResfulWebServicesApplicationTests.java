d27fd6657835013e280a626ef4cfb98818bdcd65
Venkateshwaran <venkateshawran.venkitachalam@veneratech.com>
2026-03-02 17:57:03 +0530

    Initial commit

---
+++ src/test/java/com/venkatesh/rest/webservices/resful_web_services/ResfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venkatesh.rest.webservices.resful_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
